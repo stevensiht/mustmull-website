ba63c2e81a94204d6f5c87f4df1bc6252e664c65
Steven Siht <stevensiht@gmail.com>
2025-11-28 15:19:50 +0200

    add analytics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,19 @@
-import { SiteFooter } from "@/components/footer";
-import { ContactSection } from "@/components/sections/contact";
-import { GallerySection } from "@/components/sections/gallery";
-import { HeroSection } from "@/components/sections/hero";
-import { ServicesSection } from "@/components/sections/services";
+import { SiteFooter } from '@/components/footer';
+import { ContactSection } from '@/components/sections/contact';
+import { GallerySection } from '@/components/sections/gallery';
+import { HeroSection } from '@/components/sections/hero';
+import { ServicesSection } from '@/components/sections/services';
 // import { SignatureSection } from "@/components/sections/signature";
-import { StorySection } from "@/components/sections/story";
-import { TestimonialsSection } from "@/components/sections/testimonials";
+import { StorySection } from '@/components/sections/story';
+import { TestimonialsSection } from '@/components/sections/testimonials';
+import { Analytics } from '@vercel/analytics/next';
 
-export const dynamic = "force-static";
+export const dynamic = 'force-static';
 
 export default function Home() {
   return (
     <>
-      <main className="flex flex-col">
+      <main className='flex flex-col'>
         <HeroSection />
         <StorySection />
         <ServicesSection />
@@ -22,6 +23,7 @@ export default function Home() {
         <ContactSection />
       </main>
       <SiteFooter />
+      <Analytics />
     </>
   );
 }
